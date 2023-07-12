76f3c010e6e3b720f51e7c29cc3102c0177571ef
dcazenav <92384123+dcazenav@users.noreply.github.com>
2023-07-12 15:43:21 -0400

    ajout emboss

---
+++ installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mamba create -n tb-profiler -c conda-forge -c bioconda docxtpl tb-profiler=4.4.2
+conda install -c bioconda miru-hero
+conda install -c bioconda emboss
+conda activate tb-profiler
+
+git clone https://github.com/phglab/MIRUReader.git
+git clone https://github.com/matnguyen/SpoTyping.git
+
+pip install -r requirements.txt
