f94da2b4b8aca99a1b2f6271829d225051682141
dcazenav <92384123+dcazenav@users.noreply.github.com>
2023-07-12 16:57:02 -0400

    mamba install

--- FASTAsimpi/installer.sh
+++ FASTAsimpi/installer.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mamba create -n tb-profiler -c conda-forge -c bioconda docxtpl tb-profiler=4.4.2
+conda install -c conda-forge mamba
+
 conda install -c bioconda miru-hero
 conda install -c bioconda emboss
 conda activate tb-profiler
+
+mamba create -n tb-profiler -c conda-forge -c bioconda docxtpl tb-profiler=4.4.2
 
 git clone https://github.com/phglab/MIRUReader.git
 git clone https://github.com/matnguyen/SpoTyping.git
