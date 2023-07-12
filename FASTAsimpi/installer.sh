3089151bb7f12e22b5bec2ed77a4667f1fea0969
dcazenav <92384123+dcazenav@users.noreply.github.com>
2023-07-12 15:27:11 -0400

    delete unusefull row

--- FASTAsimpi/installer.sh
+++ FASTAsimpi/installer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mamba create -n tb-profiler -c conda-forge -c bioconda docxtpl tb-profiler=4.4.2
-conda install -c bioconda miru-hero 
-conda install -c bioconda tb-profiler
+conda install -c bioconda miru-hero
 conda activate tb-profiler
 
 git clone https://github.com/phglab/MIRUReader.git
